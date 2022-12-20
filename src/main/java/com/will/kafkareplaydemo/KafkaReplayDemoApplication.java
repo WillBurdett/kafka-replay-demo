871a489ecab462b4a4de5983bbcde3c0b2024310
WillBurdett <williamburdett1995@gmail.com>
2022-12-20 20:06:06 +0000

    init

---
+++ src/main/java/com/will/kafkareplaydemo/KafkaReplayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.will.kafkareplaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReplayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaReplayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/kafkareplaydemo/KafkaReplayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.kafkareplaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaReplayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
