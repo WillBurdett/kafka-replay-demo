ed892d91006132d138677935fd79700bf962d161
WillBurdett <williamburdett1995@gmail.com>
2022-12-23 13:09:10 +0000

    returns list to postman

--- src/main/java/com/will/kafkareplaydemo/controller/KafkaBrokerController.java
+++ src/main/java/com/will/kafkareplaydemo/controller/KafkaBrokerController.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
 
 @RestController
 public class KafkaBrokerController {
@@ -34,22 +37,25 @@ public class KafkaBrokerController {
     }
 
     @GetMapping(path = "/replay-all")
-    public String listenGroupMessage() throws JsonProcessingException {
+    public List<ExampleEntity> listenGroupMessage() throws JsonProcessingException, InterruptedException {
+        // set properties for our new consumer that listens to our exiting topic
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:29092");
-        props.setProperty("group.id", "test");
+        props.setProperty("group.id", "replay-group");
         props.setProperty("enable.auto.commit", "true");
         props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("auto.offset.reset", "earliest");
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 
-        TopicPartition tp = new TopicPartition("exampleTopic", 0);
-        List<TopicPartition> topics = Arrays.asList(tp);
+        TopicPartition t = new TopicPartition("exampleTopic", 0);
+        List <TopicPartition> topics = Arrays.asList(t);
         consumer.assign(topics);
-        consumer.seek(tp, 0L);
-
+        consumer.seek(t, 0L);
         ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
         List <ExampleEntity> allExampleEntities = new ArrayList<>();
             for (ConsumerRecord<String, String> record : records) {
                 // ObjectMapper instantiation
@@ -62,9 +68,6 @@ public class KafkaBrokerController {
                 allExampleEntities.add(exampleEntity);
             }
             Collections.sort(allExampleEntities);
-            for (ExampleEntity e : allExampleEntities){
-                System.out.println(e.toString());
-            }
-            return null;
+            return allExampleEntities;
         }
 }
